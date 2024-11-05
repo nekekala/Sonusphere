879911b2e0f2f425cddd5554e1e29cb97ced6d0d
nekekala <gregpoulos1961107@gmail.com>
2024-11-05 13:19:01 +0200

    responsive header

--- wp-content/themes/sonusphere_lukostemplate/footer.php
+++ wp-content/themes/sonusphere_lukostemplate/footer.php
@@ -7,17 +7,30 @@
 		<footer class="site-footer">
 			<div class="container">
 				<div class="footer-content">
-					<h2>Sonusphere</h2>
+					<h3>Sonusphere</h3>
 					<p>Group based in Berlin, Germany<br>
 					Email: info@example.com<br>
 					Phone: +1234567890</p>
 				</div>
-				<div class="footer-copyright">
-					<h2><span class= "signature">Neke Kala</span> - Creative Commons</h2>
-				</div>
+			</div>
+			<div class="social-media-icons">
+				<a href="https://facebook.com/yourpage" target="_blank">
+					<i class="fa fa-facebook"></i> <!-- Font Awesome Facebook icon -->
+				</a>
+				<a href="https://twitter.com/yourpage" target="_blank">
+					<i class="fa fa-twitter"></i> <!-- Font Awesome Twitter icon -->
+				</a>
+				<a href="https://instagram.com/yourpage" target="_blank">
+					<i class="fa fa-instagram"></i> <!-- Font Awesome Instagram icon -->
+				</a>
+				<a href="https://linkedin.com/in/yourprofile" target="_blank">
+					<i class="fa fa-linkedin"></i> <!-- Font Awesome LinkedIn icon -->
+				</a>
+			</div>
+			<div class="footer-copyright">
+				<span class= "signature">Developed by <a href="https:/www.linkedin.com/in/grigorispoulos" rel="Neke Kala"> Neke Kala</a> - Creative Commons</span>
 			</div>
 		</footer>
-	</div>
 
 	<?php wp_footer(); ?>
 
