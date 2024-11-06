8d9e36728dc67d7ca69dede62aef38250a383e6b
nekekala <gregpoulos1961107@gmail.com>
2024-11-06 15:48:59 +0200

    header scaling for mobiles

--- wp-content/themes/sonusphere_lukostemplate/header.php
+++ wp-content/themes/sonusphere_lukostemplate/header.php
@@ -20,25 +20,20 @@
 		<!--[if IE]>
 		<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
 		<![endif]-->
-
-		<div id="app">
-			<header class="site-header">
-			    <div class="header-background" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/header_background.jpg');">
-        			<div class="logo-container">
-						<div class="site-name">
-							<div class="logo">
-								<a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
-									<img src= "<?php echo get_template_directory_uri()?> /assets/images/logo.jpg";>
-								</a>
-							</div>
-							<div class=description>
-								<h1 class="site-title"><?php bloginfo('name'); ?></h1>
-								<p class="site-description"><?php bloginfo('description'); ?></p>
-							</div>
+		<header class="site-header">
+			<div class="header-background" style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/header_background.jpg');">
+        		<div class="logo-container">
+						<div class="logo">
+							<a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
+								<img src= "<?php echo get_template_directory_uri()?> /assets/images/logo.jpg";>
+							</a>
 						</div>
-					</div>
-    			</div>
-			</header>
-		</div>
+						<div class=description>
+							<h1 class="site-title"><?php bloginfo('name'); ?></h1>
+							<p class="site-description"><?php bloginfo('description'); ?></p>
+						</div>
+				</div>
+			</div>
+		</header>
 	</body>
 			
